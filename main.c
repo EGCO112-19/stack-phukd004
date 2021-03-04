1c4bb24d00a224dcb4f9c4c0b4fa307e6a452486
phukd004 <puwadol.khm@student.mahidol.edu>
2021-03-04 07:31:06 +0000

    finish

--- main.c
+++ main.c
@@ -5,25 +5,56 @@
 
 int main(int argc, char **argv){
   printf("Checking the parentheses in argv arguments\n");
-  int i,N,j;
-  
+  int i,N,j,check=0;
+  char n;
+
   Stack s;
+  s.size = 0;
+  s.top = NULL;
 
-
- 
   for(i=1;i<argc;i++){
    
      for(j=0;j<strlen(argv[i]);j++){
        /* Use stack to help with the parentheses*/
+       switch( argv[i][j] ){
+         case '{':
+          push(&s,'{');
+          break;
+         
+         case '}':
+          push(&s,'[');
+          break;
+         
+         case ']':
+          n = pop(&s);
+          if( n!='[' ){
+            check = 1;
+          }
+          break;
 
+         case '}':
+          n = pop(&s);
+          if( n!='{' ){
+            check=1;
+          }
+          break;
 
+       }
+       if( check!=0 ) {
+         break;
+       }  
      }
 
-
+     printf("\n The size is : %d and check : %d\n",s.size,check);
+     if(s.size >0 || check!=0){
+       printf("The parentheses do not match for '%s'\n",argv[i]);
+     }
+     else{
+       printf("The parentheses match successfully for '%s'\n",argv[i]);
+     }
+     check =0;
+     s.size =0;
+     pop_all(&s);
   }
-
-
-
-
    return 0;
 }
--- stack.h
+++ stack.h
@@ -8,7 +8,41 @@ typedef struct {
 }Stack;
 
 typedef Stack * StackPtr;
-void push(StackPtr s, char value);
-char pop(StackPtr s);
-void pop_all(StackPtr s);
+
+void push(StackPtr s, char value){
+  Node *new_node=(Node) malloc(sizeof(Node));
+  if(new_node){
+    new_node->data = value;
+    new_node->nextPtr = s->top;
+    
+    s->top=new_node;
+    s->size++;
+  }
+}
+
+char pop(StackPtr s){
+  NodePtr t = s->top;
+  char value;
+  if( s->top != NULL ){
+    value = t->data;
+    s->top = t->nextPtr;
+    free(t);
+    s->size--;
+  }
+  return value;
+}
+
+void pop_all(StackPtr s){
+  NodePtr t = s->top;
+  char value;
+  while( s->top ){
+    value = t->data;
+    printf("popping : '%c'\n",value);
+    s->top = t->nextPtr;
+    free(t)
+    t = s->top;
+  }
+  s->size=0;
+}
+
 #endif
